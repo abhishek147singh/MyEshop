b5b5ffc6bd4ff0cb5ad703b3ba979856f66162f5
abhishek147singh <83961045+abhishek147singh@users.noreply.github.com>
2022-11-28 22:47:13 -0800

    Update ProductRoutes.js
    again update the path to upload the file in right directory

--- backend/routes/ProductRoutes.js
+++ backend/routes/ProductRoutes.js
@@ -6,7 +6,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'build/images')
+      cb(null, 'world/build/images')
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname)
